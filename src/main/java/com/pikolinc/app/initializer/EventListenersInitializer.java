7140bc0cb67fbb0b6569bc4ba71b074fcae5bef2
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-10-30 17:29:20 -0600

    chore: removed redundant log

--- src/main/java/com/pikolinc/app/initializer/EventListenersInitializer.java
+++ src/main/java/com/pikolinc/app/initializer/EventListenersInitializer.java
@@ -3,16 +3,11 @@ package com.pikolinc.app.initializer;
 import com.pikolinc.infraestructure.events.listeners.EventListener;
 import com.pikolinc.infraestructure.events.listeners.ItemsEventListener;
 import com.pikolinc.infraestructure.events.listeners.OffersEventListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.util.List;
 
 public class EventListenersInitializer implements Initializer {
-    private static final Logger logger = LoggerFactory.getLogger(EventListenersInitializer.class);
     @Override
     public void init() {
-        logger.info("Initializing EventListeners");
-
         List<EventListener>  eventListeners = List.of(
                 new OffersEventListener(),
                 new ItemsEventListener()
