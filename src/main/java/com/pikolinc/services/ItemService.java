19383cddbf10efe9b19e29938735c9a6ed957514
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-11-02 13:47:20 -0700

    docs: add javadoc to Item service interface

--- src/main/java/com/pikolinc/services/ItemService.java
+++ src/main/java/com/pikolinc/services/ItemService.java
@@ -5,6 +5,11 @@ import com.pikolinc.dto.request.ItemCreateDto;
 
 import java.util.List;
 
+/**
+ * Service interface for Item operations.
+ *
+ * @see com.pikolinc.services.impl.ItemServiceImpl
+ */
 public interface ItemService {
     long insert(ItemCreateDto dto);
     List<Item> findAll();
